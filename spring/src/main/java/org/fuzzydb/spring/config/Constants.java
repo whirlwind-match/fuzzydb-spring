e41b17f0992ce8a062deea96bb8b06a6036ca2bc
Neale Upstone <neale@nealeupstone.com>
2012-08-28 17:52:34 +0100

    Moved spring modules from fuzzydb repository

---
+++ spring/src/main/java/org/fuzzydb/spring/config/Constants.java
@@ -0,0 +1,8 @@
+package org.fuzzydb.spring.config;
+
+public abstract class Constants {
+
+	public static final String DEFAULT_STORE_ID = "store";
+	public static final String DEFAULT_REPO_ID = "repository";
+
+}
